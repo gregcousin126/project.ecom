02711af236d347602644d9c31f62ed2219101aaa
Greg Cousin <gregcousin126@gmail.com>
2020-06-26 11:50:35 +0200

    first commit

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -10,8 +10,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
 
-// const HOST = process.env.HOST
-// const PORT = process.env.PORT && Number(process.env.PORT)
+const HOST = process.env.HOST
+const PORT = process.env.PORT && Number(process.env.PORT)
 
 const devWebpackConfig = merge(baseWebpackConfig, {
 	module: {rules: utils.styleLoaders({sourceMap: config.dev.cssSourceMap, usePostCSS: true})},
@@ -29,10 +29,8 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 		hot: true,
 		contentBase: false, // since we use CopyWebpackPlugin.
 		compress: true,
-		// host: HOST || config.dev.host,
-		// port: PORT || config.dev.port,
-		host: config.dev.host,
-		port: config.dev.port,
+		host: HOST || config.dev.host,
+		port: PORT || config.dev.port,
 		open: config.dev.autoOpenBrowser,
 		overlay: config.dev.errorOverlay ? {warnings: false, errors: true} : false,
 		publicPath: config.dev.assetsPublicPath,
@@ -47,20 +45,23 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 		new webpack.DefinePlugin({'process.env': require('../config/dev.env')}), new webpack.HotModuleReplacementPlugin(),
 		new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
 		new webpack.NoEmitOnErrorsPlugin(), // https://github.com/ampedandwired/html-webpack-plugin
-		new HtmlWebpackPlugin({filename: 'index.html', template: 'index.html', inject: true}), // copy custom static assets
+		new HtmlWebpackPlugin({filename: 'index.html', template: 'index.html', inject: true}),// copy custom static assets
 		new CopyWebpackPlugin([{from: path.resolve(__dirname, '../static'), to: config.dev.assetsSubDirectory, ignore: ['.*']}])
 	]
 })
 
 module.exports = new Promise((resolve, reject) => {
-  portfinder.basePort =  config.dev.port // process.env.PORT ||
+  portfinder.basePort = process.env.PORT || config.dev.port
 	portfinder.getPort((err, port) => {
 	if (err) {
 		reject(err)
-	} else { // publish the new Port, necessary for e2e tests
-		config.dev.port = port // add port to devServer config
-		// process.env.PORT = port // add port to devServer config
-		devWebpackConfig.devServer.port = port // Add FriendlyErrorsPlugin
+	} else {
+		// publish the new Port, necessary for e2e tests
+		process.env.PORT = port
+		// add port to devServer config
+		devWebpackConfig.devServer.port = port
+
+		// Add FriendlyErrorsPlugin
 		devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
 			compilationSuccessInfo: {
 				messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
