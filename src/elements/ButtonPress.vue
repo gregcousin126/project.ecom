73be228294f6d7bbdae79ee492740e7075744be3
Greg Cousin <gregcousin126@gmail.com>
2020-07-11 15:08:24 +0200

    added sign in

--- src/elements/ButtonPress.vue
+++ src/elements/ButtonPress.vue
@@ -13,9 +13,32 @@ export default {
 </script>
 <style scoped>
 .button-cart{display:inline-block;margin:0 auto;-webkit-border-radius:3px;-webkit-box-shadow:var(--button-boxshadow-passive-primary);-webkit-transition:-webkit-box-shadow .2s ease-in-out}
-.button-cart span{background:#9bc2f5;color:#f0f0f0;border:3px solid #a1c6f74d;-webkit-border-radius:3px;-webkit-transition:-webkit-transform .2s ease-in-out;display:inline-block;padding:1px 4px;text-transform:uppercase;font-family:inherit;font-weight:700;font-size:var(--button-font-primary)}
+.button-cart span{
+  
+      background: #9bc2f5;
+    color: #f0f0f0;
+    border: 2px solid #a1c6f74d;
+    -webkit-border-radius: 2px;
+    -webkit-transition: -webkit-transform .2s ease-in-out;
+    display: flex;
+    padding: 1px 3px;
+    word-spacing: -.5px;
+    text-transform: uppercase;
+    font-family: inherit;
+    font-weight: 700;
+    font-size: var(--button-font-primary);
+  /* background:#9bc2f5;color:#f0f0f0;border:3px solid #a1c6f74d;-webkit-border-radius:3px;-webkit-transition:-webkit-transform .2s ease-in-out;display:inline-block;padding:1px 4px;text-transform:uppercase;font-family:inherit;font-weight:700;font-size:var(--button-font-primary) */
+  
+  
+  
+  }
 .button-cart span:hover{color:(--bg-button);cursor:pointer}
 .button-cart:active{-webkit-box-shadow:var(--button-boxshadow-active-primary)}
 .button-cart:active span{-webkit-transform:translate(0,5px)}
-@media(max-width: 650px) { .button-cart span{padding:0 1px;} }
+@media(max-width: 650px) { .button-cart span{padding:0 2px;} 
+
+
+
+
+}
 </style>
