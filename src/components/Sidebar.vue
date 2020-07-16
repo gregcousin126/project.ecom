4c855bba7c1fdb0f4965a83e1a33441de89a3300
Greg Cousin <gregcousin126@gmail.com>
2020-07-16 16:43:01 +0200

    final

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -15,8 +15,8 @@
         <router-link to="/cart" ><a class="nav-link"  >
           
           <div class="cart-nav-icon">
-            
           <div v-if="noItems > 0" class="cart-link__count" id="link__count__regular">{{ noItems }}</div>
+            
           
           <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"	 viewBox="0 0 504 504" style="0 0 484 481" xml:space="preserve"><g>	<g>		<path style="fill:#B8BAC0;" d="M485.617,114.305C484.125,112.828,482.102,112,480,112c-0.031,0-0.07,0-0.109,0l-374.5,4.941			c-2.531,0.035-4.906,1.27-6.383,3.328c-1.484,2.059-1.898,4.699-1.125,7.113l62.5,195.059c1.063,3.313,4.141,5.559,7.617,5.559			h280c22.055,0,40-17.945,40-40V120C488,117.859,487.141,115.809,485.617,114.305z M208,288c0,4.418-3.578,8-8,8s-8-3.582-8-8V168			c0-4.418,3.578-8,8-8s8,3.582,8,8V288z M248,288c0,4.418-3.578,8-8,8s-8-3.582-8-8V168c0-4.418,3.578-8,8-8s8,3.582,8,8V288z			 M288,288c0,4.418-3.578,8-8,8s-8-3.582-8-8V168c0-4.418,3.578-8,8-8s8,3.582,8,8V288z M328,288c0,4.418-3.578,8-8,8s-8-3.582-8-8			V168c0-4.418,3.578-8,8-8s8,3.582,8,8V288z M368,288c0,4.418-3.578,8-8,8s-8-3.582-8-8V168c0-4.418,3.578-8,8-8s8,3.582,8,8V288z			 M408,288c0,4.418-3.578,8-8,8s-8-3.582-8-8V168c0-4.418,3.578-8,8-8s8,3.582,8,8V288z M448,288c0,4.418-3.578,8-8,8s-8-3.582-8-8			V168c0-4.418,3.578-8,8-8s8,3.582,8,8V288z"/>	</g>	<g>		<circle style="fill:#5C546A;" cx="144" cy="416" r="32"/>	</g>	<g>		<circle style="fill:#5C546A;" cx="456" cy="416" r="32"/>	</g>	<g>		<path style="fill:#8A8895;" d="M492,108H108c-0.316,0-0.614,0.069-0.924,0.093l-6.85-23.039C96.539,72.656,84.937,64,72.008,64			L72,64.001V64H56v16h16v-0.001L72.008,80c5.898,0,11.195,3.953,12.875,9.613l73.242,246.348			c3.032,10.188,8.988,18.919,16.632,25.696l-37.156,49.542c-2.656,3.535-1.938,8.551,1.602,11.199			c1.438,1.082,3.117,1.602,4.789,1.602c2.438,0,4.836-1.105,6.406-3.199l37.713-50.286c7.295,3.45,15.317,5.485,23.693,5.485H448			v40c0,4.418,3.578,8,8,8s8-3.582,8-8v-40h16c4.422,0,8-3.582,8-8s-3.578-8-8-8H211.805c-17.563,0-33.328-11.762-38.336-28.602			L114.184,132H492c6.625,0,12-5.371,12-12S498.625,108,492,108z"/>	</g>	<g>		<path style="fill:#5C546A;" d="M56,88H16C7.163,88,0,80.837,0,72v0c0-8.837,7.163-16,16-16h40c8.837,0,16,7.163,16,16v0			C72,80.837,64.837,88,56,88z"/>	</g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>
           </div>
@@ -168,17 +168,18 @@ ul{list-style:none}
 main{margin-left:4rem;padding:1rem}
 .navbar{z-index:2;position:fixed;background-color:var(--bg-primary);transition:width var(--transition-speed-primary-faster) ease-out;overflow:scroll;width:100%;-webkit-box-shadow:inset -15px 0 16px -20px rgba(0,0,0,0.68);-moz-box-shadow:inset -15px 0 16px -20px rgba(0,0,0,0.68);box-shadow:inset -15px 0 16px -20px rgba(0,0,0,0.68)}
 .cart-link__count{
-  position: absolute;
-    width: 22px;
+position: absolute;
+    width: 20px;
+    height: 20px;
     border-radius: 16px;
-    margin-top: -7px;
+    margin-top: -8px;
     z-index: 1;
     border: 2px solid var(--bg-low-opacity);
-    margin-left: 50%;
+    left: 35px;
     padding: 0;
     background: var(--blue-primary);
     color: #fff;
-    font-size: 12px;
+    font-size: 11px;
     font-weight: 600;
   
   /* position:absolute;width:22px;border-radius:16px;top:8px;z-index:1;border:2px solid var(--bg-low-opacity);left:41px;padding:0;background:var(--blue-primary);color:#fff;font-size:12px;font-weight:600 */
@@ -201,7 +202,7 @@ main{margin-left:4rem;padding:1rem}
 .logo-text{display:inline;position:absolute;left:-999px;transition:var(--transition-speed-primary-faster)}
 .navbar:hover .logo svg{transform:rotate(-180deg)}
 @media (max-width: 600px) {
-.nav-link svg{width:1.9rem;min-width:1.9rem;margin:0 2rem}
+.nav-link svg{width:1.9rem;min-width:1.9rem;margin:0 1rem}
 /* .expandable-all-products{display:none} */
 .expandable-stores{display:none}
 .nav-link{display:inline-flex;vertical-align:middle}
